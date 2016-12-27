0f4dfa74d19db7fb60669e864465369ff9a13207
Jérémie Decock <jd.jdhp@gmail.com>
2016-12-27 22:39:22 +0100

    Clean the GUI prototype.

--- jdhp/tictactoe/gui.py
+++ jdhp/tictactoe/gui.py
@@ -23,136 +23,177 @@
 
 # See: http://effbot.org/tkinterbook/canvas.htm
 
+"""
+TODO...
+"""
+
+__all__ = ['TkGUI']
+
 import tkinter as tk
 
 SQUARE_SIZE = 128 # pixels
 SQUARE_NUM = 3    # squares per side
 
-def start_button_callback():
-    start()
+class TkGUI:
+    """
+    TODO...
+    """
 
-def quit_button_callback():
-    quit()
+    def __init__(self):
+        """
+        TODO...
+        """
 
-def player1_optionmenu_callback(value):
-    print(value)
+        # GUI parameters ##############
 
-def player2_optionmenu_callback(value):
-    print(value)
+        # TODO...
 
-root = tk.Tk()
+        # Make the main window ########
 
-player_list = ["Human",
-               "Computer (very easy)"]
+        self.root = tk.Tk()                # TODO
+        self.root.resizable(False, False)  # <- Lock the size of the window
 
-player1_label = tk.Label(root, text="Player 1 (X):")
-player1_label.grid(row=0, column=0, sticky="w")
+        # Make widgets ################
 
-player1_var = tk.StringVar()
-player1_var.set(player_list[0])
-player1_optionmenu = tk.OptionMenu(root, player1_var, *player_list, command=player1_optionmenu_callback)
-player1_optionmenu.grid(row=0, column=1, sticky="we")
+        self.available_player_type_list = ["Human",
+                                           "Computer (very easy)"]
 
-##
+        # Player1 option menu
+        player1_label = tk.Label(self.root, text="Player 1 (X):")
+        player1_label.grid(row=0, column=0, sticky="w")
 
-player2_label = tk.Label(root, text="Player 2 (O):")
-player2_label.grid(row=1, column=0, sticky="w")
+        self._player1_var = tk.StringVar()
+        self._player1_var.set(self.available_player_type_list[0])
+        self.player1_optionmenu = tk.OptionMenu(self.root,
+                                           self._player1_var,
+                                           *self.available_player_type_list)
+                                           #command=self.set_player1)
+        self.player1_optionmenu.grid(row=0, column=1, sticky="we")
 
-player2_var = tk.StringVar()
-player2_var.set(player_list[0])
-player2_optionmenu = tk.OptionMenu(root, player2_var, *player_list, command=player2_optionmenu_callback)
-player2_optionmenu.grid(row=1, column=1, sticky="we")
+        # Player2 option menu
+        player2_label = tk.Label(self.root, text="Player 2 (O):")
+        player2_label.grid(row=1, column=0, sticky="w")
 
-##
+        self._player2_var = tk.StringVar()
+        self._player2_var.set(self.available_player_type_list[0])
+        self.player2_optionmenu = tk.OptionMenu(self.root,
+                                           self._player2_var,
+                                           *self.available_player_type_list)
+                                           #command=self.set_player2)
+        self.player2_optionmenu.grid(row=1, column=1, sticky="we")
 
-canvas = tk.Canvas(root,
-                   width=SQUARE_NUM*SQUARE_SIZE,
-                   height=SQUARE_NUM*SQUARE_SIZE)
-canvas.grid(row=2, column=0, columnspan=2, sticky="nswe")
+        # Canvas
+        self.canvas = tk.Canvas(self.root,
+                                width=SQUARE_NUM*SQUARE_SIZE,
+                                height=SQUARE_NUM*SQUARE_SIZE)
+        self.canvas.grid(row=2, column=0, columnspan=2, sticky="nswe")
 
-##
+        self.canvas.tag_bind("square",             # a tag string or an item id
+                             "<Button-1>",         # the event descriptor
+                             self.canvas_callback, # the callback function
+                             add="+")              # "+" to add this binding to the previous one(s) (i.e. keep the previous binding(s)) or None to replace it or them
 
-# Start / quit / restart button
+        # Start / Quit / Restart button
+        self.button = tk.Button(self.root, text="Start")
+        self.button.grid(row=3, column=0, columnspan=2, sticky="we")
 
-button = tk.Button(root, text="Start", command=start_button_callback)
-button.grid(row=3, column=0, columnspan=2, sticky="we")
+        # Set the initial state #######
 
-for row_index in range(SQUARE_NUM):
-    for col_index in range(SQUARE_NUM):
-        color = "white"
-        tags = ("square", "{}x{}".format(col_index, row_index))
+        self.current_state = None
 
-        canvas.create_rectangle(SQUARE_SIZE * col_index,       # x1
-                                SQUARE_SIZE * row_index,       # y1
-                                SQUARE_SIZE * (col_index + 1), # x2
-                                SQUARE_SIZE * (row_index + 1), # y2
-                                tag=tags,
-                                fill=color,
-                                activefill="firebrick3",
-                                #activeoutline="firebrick1", # does not work if "width=0"
-                                #activewidth=4,              # does not work if "width=0"
-                                width=0)
+    ###################################
 
-    for col_index in range(1, SQUARE_NUM):
-        canvas.create_line((SQUARE_SIZE * col_index, 0, SQUARE_SIZE * col_index, SQUARE_SIZE * SQUARE_NUM),      # coordinates: (x1, y1, x2, y2)
-                           fill="black",
-                           width=8)
+    def run(self):
+        # Tk event loop
+        # TODO ???
+        self.init()
+        self.root.mainloop()
 
-    for row_index in range(1, SQUARE_NUM):
-        canvas.create_line((0, SQUARE_SIZE * row_index, SQUARE_SIZE * SQUARE_NUM, SQUARE_SIZE * row_index),      # coordinates: (x1, y1, x2, y2)
-                           fill="black",
-                           width=8)
+    ###################################
 
-# Bind items ##################################################################
+    def init(self):
+        self.current_state = None   # TODO: game.getInitialState()
+        self.draw_current_state()
 
-def callback(event):               # event is a tkinter.Event object
-    id_tuple = canvas.find_withtag("current")  # get the item which is under the mouse cursor
+        self.button["text"] = "Start"
+        self.button["command"] = self.start
+        self.unlock_player_option_menus()
+        self.lock_grid()
 
-    if len(id_tuple) > 0:
-        item_id = id_tuple[0]
+    def start(self):
+        self.button["text"] = "Quit"
+        self.button["command"] = self.init
+        self.lock_player_option_menus()
+        self.unlock_grid()
 
-        #print(canvas.gettags(item_id))
-        item_tag1, item_tag2, item_tag3 = canvas.gettags(item_id)
+    def lock_player_option_menus(self):
+        self.player1_optionmenu["state"] = "disabled"
+        self.player2_optionmenu["state"] = "disabled"
 
-        print("You have clicked on the square {} (item #{})".format(item_tag2, item_id))
-        print([int(coord) for coord in item_tag2.split("x")])
-    else:
-        raise Exception("Unexpected error")
+    def unlock_player_option_menus(self):
+        self.player1_optionmenu["state"] = "normal"
+        self.player2_optionmenu["state"] = "normal"
 
-canvas.tag_bind("square",          # a tag string or an item id
-                "<Button-1>",      # the event descriptor
-                callback,          # the callback function
-                add="+")           # "+" to add this binding to the previous one(s) (i.e. keep the previous binding(s)) or None to replace it or them
+    def lock_grid(self):
+        self.canvas["state"] = "disabled"
 
-###############################################################################
+    def unlock_grid(self):
+        self.canvas["state"] = "normal"
 
-def lock_player_option_menus():
-    player1_optionmenu["state"] = "disabled"
-    player2_optionmenu["state"] = "disabled"
+    ###################################
 
-def unlock_player_option_menus():
-    player1_optionmenu["state"] = "normal"
-    player2_optionmenu["state"] = "normal"
+    def get_player1_type(self):
+        return self._player1_var.get()
 
-def lock_grid():
-    canvas["state"] = "disabled"
+    def get_player2_type(self):
+        return self._player2_var.get()
 
-def unlock_grid():
-    canvas["state"] = "normal"
+    ###################################
 
-def start():
-    button["text"] = "Quit"
-    button["command"] = quit_button_callback
-    lock_player_option_menus()
-    unlock_grid()
+    def canvas_callback(self, event):                   # event is a tkinter.Event object
+        id_tuple = self.canvas.find_withtag("current")  # get the item which is under the mouse cursor
 
-def quit():
-    button["text"] = "Start"
-    button["command"] = start_button_callback
-    unlock_player_option_menus()
-    lock_grid()
+        if len(id_tuple) > 0:
+            item_id = id_tuple[0]
 
-# Main loop ###################################################################
+            #print(self.canvas.gettags(item_id))
+            item_tag1, item_tag2, item_tag3 = self.canvas.gettags(item_id)
 
-quit()
-root.mainloop()
+            print("You have clicked on the square {} (item #{})".format(item_tag2, item_id))
+            print([int(coord) for coord in item_tag2.split("x")])
+        else:
+            raise Exception("Unexpected error")
+
+    def draw_current_state(self):
+        for row_index in range(SQUARE_NUM):
+            for col_index in range(SQUARE_NUM):
+                color = "white"
+                tags = ("square", "{}x{}".format(col_index, row_index))
+
+                self.canvas.create_rectangle(SQUARE_SIZE * col_index,       # x1
+                                        SQUARE_SIZE * row_index,       # y1
+                                        SQUARE_SIZE * (col_index + 1), # x2
+                                        SQUARE_SIZE * (row_index + 1), # y2
+                                        tag=tags,
+                                        fill=color,
+                                        activefill="firebrick3",
+                                        #activeoutline="firebrick1", # does not work if "width=0"
+                                        #activewidth=4,              # does not work if "width=0"
+                                        width=0)
+
+            for col_index in range(1, SQUARE_NUM):
+                self.canvas.create_line((SQUARE_SIZE * col_index, 0, SQUARE_SIZE * col_index, SQUARE_SIZE * SQUARE_NUM),      # coordinates: (x1, y1, x2, y2)
+                                   fill="black",
+                                   width=8)
+
+            for row_index in range(1, SQUARE_NUM):
+                self.canvas.create_line((0, SQUARE_SIZE * row_index, SQUARE_SIZE * SQUARE_NUM, SQUARE_SIZE * row_index),      # coordinates: (x1, y1, x2, y2)
+                                   fill="black",
+                                   width=8)
+
+if __name__ == '__main__':
+    gui = TkGUI()
+
+    # Launch the main loop
+    gui.run()
+
